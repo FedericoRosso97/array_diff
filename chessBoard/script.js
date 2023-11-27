d5234c8c196dc5fd76bc6a7931c345c93fef3127
FedericoRosso97 <federicorosso97@gmail.com>
2023-11-27 16:58:07 +0100

    chessBoard

---
+++ chessBoard/script.js
@@ -0,0 +1,34 @@
+//prima soluzione
+
+function chessBoard(rows, columns) {
+    // your code goes here
+      let board=[];
+      let space;
+     
+      for(let i = 0; i<rows; i++){
+        let grid=[];
+      
+        for(let j = 0; j<columns; j++){
+            console.log(i+j,j,i)
+            console
+          if((i+j)%2 === 0){
+            space='O';
+          }else{
+            space='X';
+          };
+         
+          grid.push(space)
+        };
+        
+        board.push(grid);
+      };
+      
+      return board
+    }
+
+      console.log(chessBoard(4,3))
+
+      //soluzione più ottimale
+      function chessBoardFixed(rows, columns) {
+        return Array.from({length: rows}, (_,i) => Array.from({length: columns}, (_,j) => 'OX'[(i+j)%2]));
+      }
--- summatory/script.js
+++ summatory/script.js
@@ -1,17 +1,21 @@
 var summation = function (num) {
-    // Code here
     let sum;
+    
     if(num % 2 == 0){
       sum = num + ((num-1) * (num/2));
     }else{
       sum = num + ( (num-1) * Math.ceil(num/2 ) ) - Math.floor(num/2);
     }
-    
+
     return sum
   }
   
+
+  let int=9
+console.log(summation(int))
+
   /*
-  funzione che ritorna la sommatoria di un numero. (ad es num= 5 summation=(1+2+3+4+5)=15);
+  funzione che ritorna la sommatoria di un numero. (ad es. num= 5 output=(1+2+3+4+5)=15);
   per questa funzione ho voluto scoprire la mia formula personale, senza fare riferimento a formule matematiche già esistenti.
   Spiegerò poi il ragionamento che mi ha condotto a questa formula.
     
